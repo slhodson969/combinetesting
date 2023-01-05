f2585ec381f64e1c068f04fb069f03df80bf3121
Scott Hodson <scott@keepitswift.com>
2023-01-05 16:09:23 +0100

    Initial Commit

---
+++ CombineTesting/CombineTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineTestingApp.swift
+//  CombineTesting
+//
+//  Created by Scott Hodson on 05/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CombineTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
